b457fb217c7074c0f8121228e6bd7bfa138807d5
huangwenxia <892027354@qq.com>
2021-08-25 10:04:34 +0800

    userstore

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,27 +1,76 @@
-// import { Module } from "vuex"
-// import { GlobalDataProps } from "./index"
-// // export interface ModuleTree<R> {
-// //   [key: string]: Module<any, R>;
-// // }
-// const userModule: Module<PageStateProps, GlobalDataProps> = {
-//     state: {
-//         elements: [],
-//         currentElementsId: ""
-//     },
-//     mutations: {
-//         // elementAdd: (state: PageStateProps,data:ElementsType) => {
-//         //   state.elements.push(data)
-//         //   state.currentElementsId = data.id
-//         //
-//         // },
-//         // setCurrent: (state: PageStateProps, id: string) => {
-//         //   state.currentElementsId = id
-//         // }
-//     },
-//     getters: {
-//         // getCurrentElement: (state) => {
-//         //   return state.elements.find((module) => module.id === state.currentElementsId)
-//         // },
-//     }
-// }
-// export default userModule
+import { Module, ActionTree } from "vuex"
+import { GlobalDataProps } from "./index"
+import * as I from "@/api/interface/index"
+import $ctx from "@/utils/useGlobal"
+
+export interface UserStateProps {
+    info: I.user.InfoResponseResult
+}
+
+export interface UserActionsProps {
+    info: I.user.InfoResponseResult
+}
+export const userActions: ActionTree<UserStateProps, any> = {
+    getUserInfo({ commit }) {
+        $ctx.api.userApi
+            .getUserInfo()
+            .then((res) => {
+                commit("SET_USER", res.result)
+                $ctx.message.success("登录成功")
+                console.log("===========11111")
+
+                $ctx.router.push("/home")
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }
+}
+
+const userModule: Module<UserStateProps, GlobalDataProps> = {
+    state: {
+        info: {
+            id: "",
+            username: "",
+            email: "",
+            name: "",
+            sex: "",
+            type: "",
+            phone: "",
+            status: "",
+            lastLoginTime: "",
+            lastLoginIp: "",
+            createdAt: "",
+            updatedAt: "",
+            avatar: ""
+        }
+    },
+    mutations: {
+        SET_USER: (state: UserStateProps, info: I.user.InfoResponseResult) => {
+            state.info = info
+        }
+    },
+    getters: {
+        getUserInfo: (state) => {
+            return state.info
+        }
+    },
+    // actions:userActions,
+    actions: {
+        getUserInfo(context) {
+            $ctx.api.userApi
+                .getUserInfo()
+                .then((res) => {
+                    context.commit("SET_USER", res.result)
+                    $ctx.message.success("登录成功")
+                    console.log("===========11111")
+                    $ctx.router.push("/home")
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
+        }
+    }
+}
+
+export default userModule
