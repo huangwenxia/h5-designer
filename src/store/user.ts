fce89a91c5db666135da97f41230f9fa7f240069
huangwenxia <892027354@qq.com>
2021-09-16 00:06:20 +0800

    素材列表

--- src/store/user.ts
+++ src/store/user.ts
@@ -2,7 +2,6 @@ import { Module, ActionTree, Commit } from "vuex"
 import * as I from "@/api/interface/index"
 import { GlobalDataProps } from "@/store/index"
 import api from "@/api/index"
-
 export interface UserStateProps {
     info: I.user.InfoResponseResult
 }
@@ -15,7 +14,7 @@ export interface Actions {
     // 用户登出
     // [Action.SING_OUT]({ commit }: { commit: Commit; state: State }): Promise<void>
     // 用户信息
-    ["GET_USER_INFO"]({ commit }: { commit: Commit; state: UserStateProps }): Promise<I.user.InfoResponseResult>
+    ["GET_USER_INFO"]({ commit }: { commit: Commit; state: UserStateProps }): Promise<I.user.InfoResponseResult | undefined>
 }
 const userModule: Module<UserStateProps, GlobalDataProps> = {
     state: {
@@ -48,7 +47,7 @@ const userModule: Module<UserStateProps, GlobalDataProps> = {
         }
     },
     actions: <ActionTree<UserStateProps, GlobalDataProps> & Actions>{
-        GET_USER_INFO({ commit }) {
+        GET_USER_INFO: function ({ commit }) {
             return new Promise((resolve, reject) => {
                 api.userApi
                     .getUserInfo()
--- src/views/personal/design/material.vue
+++ src/views/personal/design/material.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="my-works">
-        <a-tabs v-model:activeKey="activeKey">
-            <a-tab-pane key="1" tab="图片">12312</a-tab-pane>
+        <a-tabs v-model:tabActiveKey="tabActiveKey">
+            <a-tab-pane key="1" tab="图片">
+                <div class="file-content">
+                    <Spin :spinning="loading">
+                        <div class="content">
+                            <ImageList :list="list" v-model="selectList" />
+                            <Empty :image="simpleImage" v-if="!loading && !list.length" />
+                        </div>
+                    </Spin>
+                    <div class="page">
+                        <Pagination v-model:current="listQuery.page" :total="total" @change="loadData" />
+                    </div>
+                </div>
+            </a-tab-pane>
             <a-tab-pane key="2" tab="视屏">Content of Tab Pane 2</a-tab-pane>
             <a-tab-pane key="3" tab="音乐">Content of Tab Pane 3</a-tab-pane>
             <template #tabBarExtraContent style="line-height: 1">
@@ -13,14 +25,53 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue"
+import { defineComponent, onMounted, Ref, ref } from "vue"
+import { Pagination, Empty, Spin } from "ant-design-vue"
+import ImageList from "@/components/FileService/modules/ImageList.vue"
 import BtnUpload from "@/components/upload/BtnUpload.vue"
+import * as I from "@/api/interface"
+import api from "@/api"
+
 export default defineComponent({
     name: "material",
-    components: { BtnUpload },
+    components: { BtnUpload, Pagination, Empty, Spin, ImageList },
     setup() {
+        const loading = ref(false)
+        const total: Ref<number | string> = ref(0)
+        const list: Ref<Array<I.file.baseRow>> = ref([])
+        onMounted(() => {
+            setTimeout(() => {
+                loadData()
+            })
+        })
+        const listQuery: Ref<I.file.IFileList> = ref({
+            page: 1,
+            pageSize: 12,
+            type: "image"
+        })
+
+        const loadData = () => {
+            loading.value = true
+            api.fileApi
+                .fileList(listQuery.value)
+                .then((res) => {
+                    list.value = res.result.rows
+                    total.value = res.result.count
+                })
+                .finally(() => {
+                    loading.value = false
+                })
+        }
+        const selectList: Ref<Array<I.file.baseRow>> = ref([])
         return {
-            activeKey: ref("1")
+            tabActiveKey: ref("1"),
+            list,
+            selectList,
+            total,
+            loading,
+            listQuery,
+            loadData,
+            simpleImage: Empty.PRESENTED_IMAGE_SIMPLE
         }
     }
 })
